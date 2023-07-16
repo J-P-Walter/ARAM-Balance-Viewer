1c848d4e69fc9366805d8fe5c5ab884c93e4b913
JP <j.p.walter@outlook.com>
2023-07-16 17:57:24 -0400

    Added logging and comments

--- main.py
+++ main.py
@@ -4,26 +4,34 @@ import json
 import tkinter as tk
 from queue import Queue
 import client
+import logging
 
 isARAM = False
 wasChampSelect = False
 
+logging.basicConfig(filename="error.log", level=logging.ERROR, format='%(asctime)s %(message)s')
+
 client = client.Client()
 
+#Fires when the connection is established, ie. the client is open
+#Gets the player's unique id to be used as an index later
 connector = Connector()
 @connector.ready
 async def connect(connection):
-    print('LCU API is ready to be used.')
+    print("LCU API is ready to be used.")
     player = await connection.request('get', '/lol-summoner/v1/current-summoner')
     if player.status != 200:
-        print("player id err")
+        logging.error(player)
         return
     res = json.loads(await player.read())
 
     client.setSummonerId(res['summonerId'])
-    print(f'Missing data from {len(client.getChampIds())-len(client.getChampData())} champions due to wiki inaccuracies')
+    # print(f'Missing data from {len(client.getChampIds())-len(client.getChampData())} champions due to wiki inaccuracies')
     q.put(res['summonerId'])
 
+#Fires during changes of state in the client
+#Ex. None, Lobby, ChampSelect, etc.
+#Only interested in ChampSelect and if the gamemode is ARAM
 @connector.ws.register('/lol-gameflow/v1/gameflow-phase', event_types=('UPDATE',))
 async def aramCheck(connection, event):
     await champSelect(connection)
@@ -31,30 +39,37 @@ async def aramCheck(connection, event):
 async def champSelect(connection):
     lobby = await connection.request('get', '/lol-gameflow/v1/gameflow-phase')
     if lobby.status != 200:
-        print("Err: /lol-gameflow/v1/gameflow-phase")
+        logging.error(lobby)
         return
     res = json.loads(await lobby.read())
-    print(res)
+    # print(res)
     global isARAM
     global wasChampSelect
     if res == 'ChampSelect':
         gamemode = await connection.request('get', '/lol-gameflow/v1/session')
         if gamemode.status != 200:
-            print("Err: /lol-gameflow/v1/session")
+            # print("Err: /lol-gameflow/v1/session")
+            logging.error(gamemode)
             return
         res = json.loads(await gamemode.read())
         if res['map']['gameMode'] == 'ARAM':
             isARAM = True
-        isARAM = True #TODO REMOVE FOP PROD
+        # isARAM = True #TODO REMOVE FOR PROD
         wasChampSelect = True
     else:
+        #takes care of someone dodging as well as game going through
+        #to add the stop command to the queue
         if wasChampSelect:
             q.put("stop")
             wasChampSelect = False
-        client.setSummonerIdx(-1)
+        client.setSummonerIdx(-1) #Resets variables in client
         client.setCurrentChamp(-1)
         isARAM = False
-        
+
+#Fires when isARAM is true
+#Adds gui in its own thread, making sure to not add multiple
+#Gets the current champ using data from the client
+#Adds the data to the queue used by the threads
 @connector.ws.register('/lol-champ-select/v1/session', event_types=('UPDATE',))
 async def characterInfo(connection, event):
     if not isARAM:
@@ -69,8 +84,9 @@ async def sessionInfo(connection):
 
     session = await connection.request('get', '/lol-champ-select/v1/session')
     if session.status != 200:
-        print("Err: /lol-champ-select/v1/session")
-        print(session.status)
+        # print("Err: /lol-champ-select/v1/session")
+        # print(session.status)
+        logging.ERROR(session)
         return
     res = json.loads(await session.read())
     team = res['myTeam']
@@ -88,18 +104,23 @@ async def sessionInfo(connection):
         champName = client.getChampIds()[client.getCurrentChamp()].lower()
         try:
             data = client.getChampData()[champName]
-            print(data)
+            # print(data)
             q.put(data)
         except:
             q.put("Perfectly balanced or missing from wiki lol")
             # print("Perfectly balanced or missing from wiki lol")
 
+#Start the driver
 def driver():
-    print("driver thread starting")
+    print("Driver thread starting")
     connector.start()
 
+#Start the gui
 def gui():
     window = tk.Tk()
+
+    window.geometry("200x350")
+
     nameLabel = tk.Label(text="Name: ")
     nameData = tk.Label(text="")
     dealtLabel = tk.Label(text="Damage Dealt: ")
@@ -107,7 +128,7 @@ def gui():
     receivedLabel = tk.Label(text="Damage Taken: ")
     receivedData = tk.Label(text="")
     effectsLabel = tk.Label(text="Other Effects: ")
-    effectsData = tk.Label(text="")
+    effectsData = tk.Label(text="", wraplength=100, justify='center')
     
     nameLabel.grid(row = 0, column = 0)
     nameData.grid(row = 0, column = 1)
@@ -116,14 +137,17 @@ def gui():
     receivedLabel.grid(row = 2, column = 0)
     receivedData.grid(row = 2, column = 1)
     effectsLabel.grid(row = 3, column = 0)
-    effectsData.grid(row = 4, column = 1)
+    effectsData.grid(row = 3, column = 1)
 
-    print("gui thread starting")
+    print("Gui thread starting")
 
+    #Checks the queue for data
+    #Updates accordingly
+    #Currently checks every 100ms which seems to be a reasonable amount
     def check_queue():
         try:
             res = q.get(0)
-            print(res)
+            # print(res)
             if res == "stop":
                 stop_gui()
             if isinstance(res, str):
@@ -137,7 +161,8 @@ def gui():
                 receivedData.config(text=res['damageReceived'])
                 effectsData.config(text=res['otherEffects'])
         except:
-            print("queue empty")
+            # print("Queue empty")
+            pass
         finally:
             window.after(100, check_queue)
 
